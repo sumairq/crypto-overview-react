7cfddce102d63b1696818589009571f12f32e489
Sumair <sumairqaisar992@gmail.com>
2022-03-26 13:30:23 +0500

    Fix linter errrors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={elementStore}>
       <BrowserRouter>
-      <App />
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
