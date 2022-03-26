082e3cd84edd876d87f8e5c13c0556556cf63a5a
Sumair <sumairqaisar992@gmail.com>
2022-03-26 14:28:14 +0500

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import elementStore from './redux/configureStore';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
